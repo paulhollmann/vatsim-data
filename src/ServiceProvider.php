5cd9d3b6ce4a561a1a4824d2012898ca030b0236
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2024-07-13 19:27:37 +0200

    update

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,7 +8,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__.'/../config/courier.php' => config_path('courier.php'),
+            __DIR__.'/../config/courier.php' => config_path('vatsimdatafeed.php'),
         ]);
     }
 
